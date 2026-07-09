dd7c8d34f556013faf1e3a3ac928765fe283a8cd
Ajithkumar-ak1 <ajithkumar.proff@gmail.com>
2026-07-09 12:41:57 +0530

    Intial Project Structure

---
+++ KnowTheRound/src/main/java/com/ajith/KnowTheRound/KnowTheRoundApplication.java
@@ -0,0 +1,13 @@
+package com.ajith.KnowTheRound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowTheRoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowTheRoundApplication.class, args);
+	}
+
+}
---
+++ KnowTheRound/src/test/java/com/ajith/KnowTheRound/KnowTheRoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajith.KnowTheRound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowTheRoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
