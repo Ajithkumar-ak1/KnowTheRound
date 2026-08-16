25dd005da2db3982f853892d6190b388670c475b
Ajithkumar-ak1 <ajithkumar.proff@gmail.com>
2026-08-16 19:51:27 +0530

    Added the frontend url

--- KnowTheRound/src/main/java/com/ajith/KnowTheRound/config/SecurityConfig.java
+++ KnowTheRound/src/main/java/com/ajith/KnowTheRound/config/SecurityConfig.java
@@ -68,7 +68,8 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of(
-                "http://localhost:5173"
+                "http://localhost:5173",
+                "https://knowtheround.vercel.app"
         ));
 
         configuration.setAllowedMethods(List.of(
